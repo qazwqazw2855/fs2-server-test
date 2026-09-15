e0f45f7cf338d3939f49f5806314149e72490d1e
qazwqazw2855 <qazwqazw2855@gmail.com>
2026-09-15 15:36:08 +0800

    Accept both recovered Server V2 movement samples

--- src/God2.ServerV2.Protocol/OfficialWorldMovementCodec.cs
+++ src/God2.ServerV2.Protocol/OfficialWorldMovementCodec.cs
@@ -4,10 +4,27 @@ public static class OfficialWorldMovementCodec
 {
     public const int FrameLength = 10;
 
-    private static readonly byte[] VerifiedRequest =
-        Convert.FromHexString("0A0080BAD7C34DA69488");
+    private static readonly byte[][] VerifiedRequests =
+    [
+        Convert.FromHexString("0A0080BAD7C34DA69488"),
+        Convert.FromHexString("0A0080BAD7C44EA79586")
+    ];
 
-    public static bool IsVerifiedRequest(ReadOnlySpan<byte> frame) =>
-        frame.Length == FrameLength &&
-        frame.SequenceEqual(VerifiedRequest);
+    public static bool IsVerifiedRequest(ReadOnlySpan<byte> frame)
+    {
+        if (frame.Length != FrameLength)
+        {
+            return false;
+        }
+
+        foreach (var verifiedRequest in VerifiedRequests)
+        {
+            if (frame.SequenceEqual(verifiedRequest))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
