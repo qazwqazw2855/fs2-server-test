050b36b96447f3c816c864e12ff230d984568c2f
qazwqazw2855 <qazwqazw2855@gmail.com>
2026-09-15 16:19:14 +0800

    Separate Server V2 bind and advertised addresses

--- src/God2.ServerV2.Network/TcpServerOptions.cs
+++ src/God2.ServerV2.Network/TcpServerOptions.cs
@@ -2,8 +2,16 @@ using System.Net;
 
 namespace God2.ServerV2.Network;
 
-public sealed record TcpServerOptions(IPAddress BindAddress, int Port)
+public sealed record TcpServerOptions(
+    IPAddress BindAddress,
+    int Port,
+    IPAddress AdvertisedAddress)
 {
+    public TcpServerOptions(IPAddress bindAddress, int port)
+        : this(bindAddress, port, bindAddress)
+    {
+    }
+
     public static TcpServerOptions Loopback(int port = 2592) =>
-        new(IPAddress.Loopback, port);
+        new(IPAddress.Loopback, port, IPAddress.Loopback);
 }
